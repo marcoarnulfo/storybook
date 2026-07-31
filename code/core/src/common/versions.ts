2bce08fe5b658f5d04fd765b712db5aed7073b7d
Michael Shilman <shilman@users.noreply.github.com>
2026-07-31 15:51:49 +0800

    Move CSF enrichment into addon-docs

--- code/core/src/common/versions.ts
+++ code/core/src/common/versions.ts
@@ -32,7 +32,6 @@ export default {
   '@storybook/codemod': '10.6.0-alpha.3',
   '@storybook/core-webpack': '10.6.0-alpha.3',
   'create-storybook': '10.6.0-alpha.3',
-  '@storybook/csf-plugin': '10.6.0-alpha.3',
   'eslint-plugin-storybook': '10.6.0-alpha.3',
   '@storybook/mcp': '10.6.0-alpha.3',
   '@storybook/react-dom-shim': '10.6.0-alpha.3',
